faf417544afa82cd51006be7216a5d1b5490b2ef
Sam Hatfield <samuel.hatfield@jesus.ox.ac.uk>
2015-12-27 18:03:59 +0000

    Added filtering loop, albeit without analysis step
    Also added first plot, and moved 'model error' stuff to `lorenz96.py`.
    This doesn't belong in `main.py`.

--- python/lorenz96.py
+++ python/lorenz96.py
@@ -1,22 +1,30 @@
-from numpy import array
+import numpy as np
 
 F = 8
 DT = 0.05
 
-def lorenz96(prev_state):
-    k1 = florenz96(prev_state)
-    k2 = florenz96(prev_state+0.5*DT*k1)
-    k3 = florenz96(prev_state+0.5*DT*k2)
-    k4 = florenz96(prev_state+DT*k3)
-
-    return prev_state + (DT/6)*(k1 + 2*k2 + 2*k3 + k4)
-
-def florenz96(prev_state):
+def lorenz96(prev_state, model_error = False):
+    # Number of X variables
     num_x = len(prev_state)
 
-    return array([dXdT(prev_state, i) for i in range(num_x)])
+    k1 = florenz96(prev_state, num_x)
+    k2 = florenz96(prev_state+0.5*DT*k1, num_x)
+    k3 = florenz96(prev_state+0.5*DT*k2, num_x)
+    k4 = florenz96(prev_state+DT*k3, num_x)
 
-def dXdT(X, k):
-    K = len(X)
+    next_state = prev_state + (DT/6)*(k1 + 2*k2 + 2*k3 + k4)
+
+    # Include normally distributed errors, if specified
+    if model_error:
+        # Model error can be modified here
+        sigma_x = np.ones(num_x)
+        return next_state + sqrt(DT) * sigma_x * np.random.randn(num_x)
+    else:
+        return next_state
+
+def florenz96(prev_state, K):
+    return np.array([dXdT(prev_state, i, K) for i in range(K)])
+
+def dXdT(X, k, K):
     return (X[(k+1)%K] - X[k-2])*X[k-1] - X[k] + F
 
--- python/main.py
+++ python/main.py
@@ -1,7 +1,13 @@
-from numpy import arange, zeros, ones, array
 from numpy.random import randn
+from numpy.linalg import norm
+import numpy as np
 from math import sqrt
 from lorenz96 import lorenz96, DT
+import matplotlib.pyplot as plt
+
+#===============================================================================
+# Setup
+#===============================================================================
 
 # Number of X variables
 num_x = 40
@@ -11,15 +17,14 @@ t1, t2 = DT, 6
 
 # Set total number of model steps (last step isn't included by 
 # arange, so it is added separately
-num_steps = len(arange(t1, t2, DT)) + 1
+num_steps = len(np.arange(t1, t2, DT)) + 1
 
-# Initialize solution arrays
-#truth = free = filter_a = filter_f = zeros((num_steps, num_x))
-truth = []
-
+#===============================================================================
 # Spin up
+#===============================================================================
+
 spins = 5000
-state = 8 * ones(num_x)
+state = 8 * np.ones(num_x)
 state[19] = 8.008
 
 for _ in range(spins):
@@ -27,15 +32,93 @@ for _ in range(spins):
 
 initial_truth = state 
 
-# Set 'model error'
-var_x = ones(num_x)
-sigma_x = array([sqrt(x) for x in var_x])
-alpha = 0.0
-
+#===============================================================================
 # Obtain truth run
+#===============================================================================
+
+truth = []
 next_x = initial_truth
 for _ in range(num_steps):
     truth.append(next_x)
-    next_x = lorenz96(next_x) + alpha*sqrt(DT)*sigma_x*randn(num_x)
+    next_x = lorenz96(next_x)
 
-print truth[-1]
+#===============================================================================
+# Extract and perturb observations
+#===============================================================================
+
+# For now, every X variable is observed
+obs = np.array(truth)
+
+# Set frequency of observations (same as DT for now) (must be multiple of DT)
+DT_obs = DT
+assert(DT_obs % DT == 0.0)
+
+var_o = 0.01 * np.ones(num_x)
+sigma_o = np.array([sqrt(x) for x in var_o])
+# r_o = 
+
+# Perturb observations
+for i in range(len(obs)):
+    obs[i] = obs[i] + sigma_o * randn(num_x)
+
+#===============================================================================
+# Setup filtering
+#===============================================================================
+
+# Ensemble size
+num_ens = 50
+
+# Number of forecast steps between each analysis step
+num_fore_steps = int(DT_obs/DT)
+
+# Total number of filter steps
+num_filt_steps = len(np.arange(t1, t2, DT_obs))
+
+# Set inflation factor
+rho = 1.4
+
+# Initialization step
+Xa0 = np.mean(truth, axis=0)
+var0 = 3 * np.ones(num_x)
+sigma0 = np.array([sqrt(x) for x in var0])
+
+# Define ensemble and perturb members
+ensemble = []
+for _ in range(num_ens):
+    ensemble.append(Xa0 + sigma0 * randn(num_x))
+
+# Store first step
+forecast_history = analysis_history = [np.mean(ensemble, axis=0)]
+
+#===============================================================================
+# Run filter
+#===============================================================================
+
+for filt_step in range(num_filt_steps):
+    print 'Step %d' % filt_step
+
+    # Forecast steps
+    for _ in range(num_fore_steps):
+        ensemble = [lorenz96(member) for member in ensemble]
+
+
+    ens_mean = np.mean(ensemble, axis=0)
+
+    # Save ensemble state prior to analysis
+    forecast_history.append(ens_mean)
+
+    print 'End forecast'
+
+#===============================================================================
+# Plot results
+#===============================================================================
+
+rms_fore = []
+for filt_step in range(num_filt_steps):
+    skip = filt_step * num_fore_steps
+
+    rms_fore.append(1.0/sqrt(num_x) * norm(truth[skip] - forecast_history[skip]))
+
+# Plot RMS differences between truth and other runs
+plt.plot(rms_fore)
+plt.show()
