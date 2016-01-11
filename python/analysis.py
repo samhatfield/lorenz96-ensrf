df3edab60a51db5d2d3cd198a7b0f0e1e0983cf5
Sam Hatfield <samuel.hatfield@jesus.ox.ac.uk>
2016-01-11 15:42:32 +0000

    Simplified analysis routine

--- python/analysis.py
+++ python/analysis.py
@@ -1,32 +1,38 @@
 import numpy as np
+from numpy import dot
 from numpy.random import randn
 from numpy.linalg import inv
 
-from observation import observe
+from observation import observe as H
 
 RHO = 1.4
 
 def assimilate(ensemble, obs_vec, Ro, sigma_o):
+    # Mean ensemble vector
     ens_mean = np.mean(ensemble, axis=1)
-    num_ens = ensemble.shape[1]
+
+    # Number of ensemble members
+    N = ensemble.shape[1]
+
+    # Dimension of observed vector
     num_obs = obs_vec.shape[0]
 
     # Table of perturbed observations. Each column contains the 
     # observation vector for this timestep with a bit of Gaussian-drawn
     # noise (different noise for each member)
-    obs_table = np.empty((num_obs, num_ens))
-    for i in range(num_ens):
+    obs_table = np.empty((num_obs, N))
+    for i in range(N):
         obs_table[:,i] = obs_vec + sigma_o * randn(num_obs)
 
     # 'Anomaly table'. Each column is that member's difference from the mean
-    Ensfp = ensemble - ens_mean[:,np.newaxis]
+    anom_table = ensemble - ens_mean[:,np.newaxis]
 
-    HEnsfp = observe(Ensfp)
+    # Ensemble covariance times transpose of observation matrix
+    ens_cov_H_T = (RHO/(N-1)) * dot(anom_table, np.transpose(H(anom_table)))
 
-    covfHt = (RHO/(num_ens-1)) * np.dot(Ensfp, np.transpose(HEnsfp))
+    # Kalman gain
+    gain = dot(ens_cov_H_T, inv(H(ens_cov_H_T) + Ro))
 
-    HcovfHt = (RHO/(num_ens-1)) * np.dot(HEnsfp, np.transpose(HEnsfp)) + Ro
-
-    Gain = np.dot(covfHt, inv(HcovfHt))
-
-    return ensemble + np.dot(Gain, obs_table - observe(ensemble))
+    # Combine previous ensemble and observations with Kalman gain to generate
+    # next ensemble
+    return ensemble + dot(gain, obs_table - H(ensemble))
