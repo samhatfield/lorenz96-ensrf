e4414e1788b1832b486b67e07a95fd87a9ed988b
Sam Hatfield <samuel.hatfield@jesus.ox.ac.uk>
2016-02-22 11:09:46 +0000

    Made the integration code a little cleaner

--- fortran/lorenz96.f90
+++ fortran/lorenz96.f90
@@ -6,9 +6,9 @@ module lorenz96
     ! Model parameters
     real(dp), parameter :: f = 20
     real(dp), parameter :: h = 1
-    real(dp), parameter :: c = 4
+    real(dp), parameter :: c = 10
     real(dp), parameter :: b = 10
-    real(dp), parameter :: e = 4
+    real(dp), parameter :: e = 10
     real(dp), parameter :: d = 10
     real(dp), parameter :: g_X = 1
     real(dp), parameter :: g_Y = c
@@ -16,40 +16,36 @@ module lorenz96
 
     contains
         ! Step forward once
-        pure function step(prev_state)
+        function step(prev_state)
             real(dp), dimension(state_dim), intent(in) :: prev_state
-            real(dp), dimension(n_x) :: x, k1, k2, k3, k4
-            real(dp), dimension(n_x*n_y) :: y, l1, l2, l3, l4
-            real(dp), dimension(n_x*n_y*n_z) :: z, m1, m2, m3, m4
-            real(dp), dimension(state_dim) :: step
+            real(dp), dimension(state_dim) :: step, k1, k2, k3, k4
 
-            x = prev_state(:n_x)
-            y = prev_state(n_x+1:n_x+n_x*n_y)
-            z = prev_state(n_x+n_x*n_y+1:)
-        
-            ! 3 coupled ODEs 4th order Runge-Kutta
-            k1 = dXdT(x, y)
-            l1 = dYdT(x, y, z)
-            m1 = dZdT(y, z)
-            k2 = dXdT(x+0.5*dt*k1, y+0.5*dt*l1)
-            l2 = dYdT(x+0.5*dt*k1, y+0.5*dt*l1, z+0.5*dt*m1)
-            m2 = dZdT(y+0.5*dt*l1, z+0.5*dt*m1)
-            k3 = dXdT(x+0.5*dt*k2, y+0.5*dt*l2)
-            l3 = dYdT(x+0.5*dt*k2, y+0.5*dt*l2, z+0.5*dt*m2)
-            m3 = dZdT(y+0.5*dt*l2, z+0.5*dt*m2)
-            k4 = dXdT(x+dt*k3, y+dt*l3)
-            l4 = dYdT(x+dt*k3, y+dt*l3, z+dt*m3)
-            m4 = dZdT(y+dt*l3, z+dt*m3)
-        
-            x = x + (dt/6)*(k1 + 2*k2 + 2*k3 + k4)
-            y = y + (dt/6)*(l1 + 2*l2 + 2*l3 + l4)
-            z = z + (dt/6)*(m1 + 2*m2 + 2*m3 + m4)
+            ! 4th order Runge-Kutta
+            k1 = ode(prev_state)
+            k2 = ode(prev_state+0.5*dt*k1) 
+            k3 = ode(prev_state+0.5*dt*k2) 
+            k4 = ode(prev_state+dt*k3) 
 
-            ! Concatenate X's and Y's into return value
-            step(:n_x) = x
-            step(n_x+1:n_x+n_x*n_y) = y
-            step(n_x+n_x*n_y+1:) = z
+            step = prev_state + (dt/6)*(k1 + 2*k2 + 2*k3 + k4)
         end function step
+
+        pure function ode(state)
+            real(dp), dimension(state_dim), intent(in) :: state
+            real(dp), dimension(n_x) :: x
+            real(dp), dimension(n_x*n_y) :: y
+            real(dp), dimension(n_x*n_y*n_z) :: z
+            real(dp), dimension(state_dim) :: ode
+
+            ! Break up state vector into components
+            x = state(:n_x)
+            y = state(n_x+1:n_x+n_x*n_y)
+            z = state(n_x+n_x*n_y+1:)
+
+            ! Find derivative of each component separately
+            ode(:n_x) = dXdT(x, y)
+            ode(n_x+1:n_x+n_x*n_y) = dYdT(x, y, z)
+            ode(n_x+n_x*n_y+1:) = dZdT(y, z)
+        end function ode
 
         ! X ODE
         pure function dXdT(x, y)
